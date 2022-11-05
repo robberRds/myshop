87426cac0d1393466f168bfbbc2e10bc69b0d62b
BalitskiyIhor <ihorbalitsky@gmail.com>
2022-11-05 21:22:22 +0200

    general pattern

---
+++ resources/views/layouts/scripts.blade.php
@@ -0,0 +1,11 @@
+<script src="../../../public/js/vendor/jquery-1.12.0.min.js"></script>
+<script src="../../../public/js/popper.js"></script>
+<script src="../../../public/js/bootstrap.min.js"></script>
+<script src="../../../public/js/jquery.counterup.min.js"></script>
+<script src="../../../public/js/waypoints.min.js"></script>
+<script src="../../../public/js/elevetezoom.js"></script>
+<script src="../../../public/js/ajax-mail.js"></script>
+<script src="../../../public/js/owl.carousel.min.js"></script>
+<script src="../../../public/js/plugins.js"></script>
+<script src="../../../public/js/main.js"></script>
+@yield('scripts')
--- routes/web.php
+++ routes/web.php
@@ -28,54 +28,9 @@ Route::get('food-shop/index', [\App\Http\Controllers\ProductController::class, '
 Route::get('food-shop/product-details', [\App\Http\Controllers\ProductController::class, 'productDetails'])
     ->name('food-shop/product-details');
 
-/*Route::get('food-shop/product-details', function () {
-    return view('food-shop/product-details');
-})->name('food-shop/product-details');*/
-
-Route::get('food-shop/index-2', function () {
-    return view('food-shop/index-2');
-})->name('food-shop/index-2');
-
 Route::get('food-shop/contact', function () {
     return view('food-shop/contact');
 })->name('food-shop/contact');
-
-Route::get('food-shop/shop-list', function () {
-    return view('food-shop/shop-list');
-})->name('food-shop/shop-list');
-
-
-Route::get('food-shop/cart', function () {
-    return view('food-shop/cart');
-})->name('food-shop/cart');
-
-Route::get('food-shop/checkout', function () {
-    return view('food-shop/checkout');
-})->name('food-shop/checkout');
-
-Route::get('food-shop/wishlist', function () {
-    return view('food-shop/wishlist');
-})->name('food-shop/wishlist');
-
-Route::get('food-shop/my-account', function () {
-    return view('food-shop/my-account');
-})->name('food-shop/my-account');
-
-Route::get('food-shop/login-register', function () {
-    return view('food-shop/login-register');
-})->name('food-shop/login-register');
-
-Route::get('food-shop/blog', function () {
-    return view('food-shop/blog');
-})->name('food-shop/blog');
-
-Route::get('food-shop/blog-details', function () {
-    return view('food-shop/blog-details');
-})->name('food-shop/blog-details');
-
-Route::get('food-shop/blog-leftsidebar', function () {
-    return view('food-shop/blog-leftsidebar');
-})->name('food-shop/blog-leftsidebar');
 
 Route::get('food-shop/about', [\App\Http\Controllers\FoodController::class, 'about'])
     ->name('food-shop/about');
@@ -89,3 +44,51 @@ require __DIR__.'/auth.php';
 Route::group(['prefix' => 'admin'], function () {
     Voyager::routes();
 });
+
+/*Route::get('food-shop/product-details', function () {
+    return view('food-shop/product-details');
+})->name('food-shop/product-details');*/
+
+Route::get('food-shop/shop-list', function () {
+    return view('food-shop/shop-list');
+})->name('food-shop/shop-list');
+
+Route::get('food-shop/cart', function () {
+    return view('food-shop/cart');
+})->name('food-shop/cart');
+
+Route::get('food-shop/checkout', function () {
+    return view('food-shop/checkout');
+})->name('food-shop/checkout');
+
+Route::get('food-shop/wishlist', function () {
+    return view('food-shop/wishlist');
+})->name('food-shop/wishlist');
+
+Route::get('food-shop/login-register', function () {
+    return view('food-shop/login-register');
+})->name('food-shop/login-register');
+
+
+
+
+
+Route::get('food-shop/my-account', function () {
+    return view('food-shop/my-account');
+})->name('food-shop/my-account');
+
+Route::get('food-shop/blog', function () {
+    return view('food-shop/blog');
+})->name('food-shop/blog');
+
+Route::get('food-shop/blog-details', function () {
+    return view('food-shop/blog-details');
+})->name('food-shop/blog-details');
+
+Route::get('food-shop/blog-leftsidebar', function () {
+    return view('food-shop/blog-leftsidebar');
+})->name('food-shop/blog-leftsidebar');
+
+Route::get('food-shop/index-2', function () {
+    return view('food-shop/index-2');
+})->name('food-shop/index-2');
